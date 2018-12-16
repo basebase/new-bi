f1c7f7da71e4ca144cf1d27fbb640f16718e0ff2
李建峰 <base_2@163.com>
2018-12-16 19:49:12 +0800

    up

--- src/pages/demo2.js
+++ src/pages/demo2.js
@@ -3,15 +3,103 @@
  */
 
 import React from 'react'
+import { WidthProvider, Responsive } from "react-grid-layout"
 import '../../assert/css/pages/demo.css'
+import _ from "lodash";
+
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+const originalLayouts = getFromLS("layouts") || {};
+
+
+function getFromLS(key) {
+    let ls = {};
+    if (global.localStorage) {
+        try {
+            ls = JSON.parse(global.localStorage.getItem("rgl-8")) || {};
+        } catch (e) {
+            /*Ignore*/
+        }
+    }
+    return ls[key];
+}
+
+function saveToLS(key, value) {
+    if (global.localStorage) {
+        global.localStorage.setItem(
+            "rgl-8",
+            JSON.stringify({
+                [key]: value
+            })
+        );
+    }
+}
+
+
 
 export default class Demo2 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            layouts: JSON.parse(JSON.stringify(originalLayouts))
+        }
+    }
+
+    static get defaultProps() {
+        return {
+            className: "layout",
+            cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+            rowHeight: 30
+        };
+    }
+
+    resetLayout() {
+        this.setState({ layouts: {} });
+    }
+
+    onLayoutChange(layout, layouts) {
+        saveToLS("layouts", layouts);
+        this.setState({ layouts });
+    }
+
     render() {
+        // layout is an array of objects, see the demo for more complete usage
+        var layout = [
+            {i: 'a', x: 0, y: 0, w: 1, h: 2, /*static: true*/},
+            {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+            {i: 'c', x: 4, y: 0, w: 1, h: 2}
+        ];
         return (
             <div>
-                <div className="room">
-                    <p>Hello React Router...</p>
-                </div>
+                <button onClick={() => this.resetLayout()}>Reset Layout</button>
+                <ResponsiveReactGridLayout
+                    className="layout"
+                    cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                    rowHeight={30}
+                    layouts={this.state.layouts}
+                    onLayoutChange={(layout, layouts) =>
+                        this.onLayoutChange(layout, layouts)
+                    }
+                >
+                    <div key="1" data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}>
+                        <span className="text">1</span>
+                    </div>
+                    <div key="2" data-grid={{ w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 }}>
+                        <span className="text">2</span>
+                    </div>
+                    <div key="3" data-grid={{ w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 }}>
+                        <span className="text">3</span>
+                    </div>
+                    <div key="4" data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}>
+                        <span className="text">4</span>
+                    </div>
+                    <div key="5" data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}>
+                        <span className="text">5</span>
+                    </div>
+                </ResponsiveReactGridLayout>
             </div>
         )
     }
